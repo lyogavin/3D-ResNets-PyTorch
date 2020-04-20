98f0c08ed77f45abb80d96494b1403ad57d120da
Gavin Li <l_y_o@pulsarsmarthome.cn>
2020-04-20 19:52:21 +0100

    fix bugs

--- run_infer.sh
+++ run_infer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 ./ActionRecognizor.py --video_jpgs_dir_path ./kinetics_dl/abseiling/xmvp3eKN7EM_000000_000010/ --n_pretrain_classes 700 --pretrain_path ./data/r3d18_K_200ep.pth --model resnet --model_depth 18  --n_classes 700
+python3 ./ActionRecognizor.py --n_val_samples 5 --annotation_path ./kinetics_dl/kinetics_700_anno.json --video_jpgs_dir_path ./test_videos/test_v3/ --n_pretrain_classes 700 --pretrain_path ./data/r3d18_K_200ep.pth --model resnet --model_depth 18  --n_classes 700
 
